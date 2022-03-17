3725b95bd2e4dc77981c129a06bddbc5c517327d
DabboussiAmal <amaldabboussi74@gmail.com>
2022-03-17 13:50:02 +0100

    component

---
+++ src/Component/Profile/Address.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Address = () => {
+  return (
+    <div> <h2>I'm from Bou Salem</h2> </div>
+  )
+}
+
+export default Address
---
+++ src/Component/Profile/ProfilePhoto.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import MyPhoto from '../../Images/MyPhoto.jpg'
+const ProfilePhoto = () => {
+  return (
+    <div>
+        <img src={MyPhoto} alt='Photo'/>
+    </div>
+  )
+}
+
+export default ProfilePhoto
